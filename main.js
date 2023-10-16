5f659064710a4840f2f5486bce752b10988b014f
iamawez <awezmanyar1672@gmail.com>
2023-10-16 15:43:57 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('main');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
